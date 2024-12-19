57e788321a610e10271d083217ed67a4462d0177
Warrenn205 <warrenspencer41@gmail.com>
2024-12-19 13:14:16 -0600

    Added Spring Boot to Project

---
+++ gameplay/src/main/java/com/demo/gameplay/GameplayApplication.java
@@ -0,0 +1,13 @@
+package com.demo.gameplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameplayApplication {
+// Gameplay
+	public static void main(String[] args) {
+		SpringApplication.run(GameplayApplication.class, args);
+	}
+
+}
